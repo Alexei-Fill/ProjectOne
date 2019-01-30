9fc776f970e96a659b673b03331df865a8002ea9
Alexey Filyutin <Alexey_Filyutin@epam.com>
2019-01-30 16:31:36 +0600

    0.014/m

--- src/main/java/com/SpEx7/config/MvcWebApplicationInitalizer.java
+++ src/main/java/com/SpEx7/config/MvcWebApplicationInitalizer.java
@@ -13,47 +13,17 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
 public class MvcWebApplicationInitalizer implements WebApplicationInitializer {
-//    @Override
-//    protected Class<?>[] getRootConfigClasses() {
-//        return new Class[] {AppConfig.class};
-//    }
-//
-//    @Override
-//    protected Class<?>[] getServletConfigClasses() {
-//        return new Class[] {WebMvcConfig.class};
-//    }
-//
-//    @Override
-//    protected String[] getServletMappings() {
-//        return new String[] {"/"};
-//    }
 
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
         AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
         context.setConfigLocation("com.SpEx7.config");
         servletContext.addListener(new ContextLoaderListener(context));
-        FilterRegistration.Dynamic encod =  servletContext.addFilter("encodingFilter", new CharacterEncodingFilter());
-        encod.setInitParameter("encoding", "UTF-8");
-        encod.setInitParameter("forceEncoding", "true");
-        encod.addMappingForServletNames(null, true, "/*");
         ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", new DispatcherServlet(context));
         dispatcher.setLoadOnStartup(1);
         dispatcher.addMapping("/");
+        FilterRegistration.Dynamic encodingFilter =  servletContext.addFilter("encodingFilter", new CharacterEncodingFilter("UTF-8", true));
+        encodingFilter.addMappingForServletNames(null, false, "dispatcher");
     }
 }
 
-
-//    public void onStartup(ServletContext container) {
-//        AnnotationConfigWebApplicationContext context
-//                = new AnnotationConfigWebApplicationContext();
-//        context.setConfigLocation("com.example.app.config");
-//
-//        container.addListener(new ContextLoaderListener(context));
-//
-//        ServletRegistration.Dynamic dispatcher = container
-//                .addServlet("dispatcher", new DispatcherServlet(context));
-//
-//        dispatcher.setLoadOnStartup(1);
-//        dispatcher.addMapping("/");
-//    }
