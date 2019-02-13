aa103182c959d799e75a7a34e3c44ea456a1d432
Alexey Filyutin <Alexey_Filyutin@epam.com>
2019-02-13 12:42:28 +0600

    0.027

--- src/main/java/com/SpEx7/config/SwaggerConfig.java
+++ src/main/java/com/SpEx7/config/SwaggerConfig.java
@@ -10,6 +10,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
@@ -23,9 +24,10 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.ant("/rest/**"))
                 .build()
+                .securitySchemes(Arrays.asList())
+                .securityContexts(Arrays.asList())
                 .apiInfo(apiInfo());
     }
-
 
     private ApiInfo apiInfo() {
         return new ApiInfo(
