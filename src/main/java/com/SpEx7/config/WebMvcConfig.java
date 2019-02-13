fa87efe65b63e38e2f72d4561a19f8c14ab8ed15
Alexey Filyutin <Alexey_Filyutin@epam.com>
2019-02-13 12:00:41 +0600

    0.026

--- src/main/java/com/SpEx7/config/WebMvcConfig.java
+++ src/main/java/com/SpEx7/config/WebMvcConfig.java
@@ -54,6 +54,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
     @Override
